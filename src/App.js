0d6a9c321ba4707ae9ae7dee2e18c4b8cd439534
Natasha <njohnson533@gmail.com>
2022-02-27 19:19:32 -0500

    added route to Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react";
 import { GlobalStyle } from "./components/App/style";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Hero from "./components/Hero/Hero";
+import Home from "./pages/Home/Home";
 import Header from "./components/Header/Header";
 import { AppContext } from "./Contexts/Context";
 
@@ -21,10 +21,13 @@ function App() {
   
   return (
     <AppContext.Provider value={""}>
+      <GlobalStyle />
       <Router>
         <Header />
-        <GlobalStyle />
-        <Hero />
+        <Routes>
+          <Route path="/" element={<Home />} />
+      
+        </Routes>
       </Router>
     </AppContext.Provider>
   );
