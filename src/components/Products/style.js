1cf6605c0664095670a06564f2165e5640c87c55
Natasha <njohnson533@gmail.com>
2022-02-28 11:43:13 -0500

    added media query for cards

--- src/components/Products/style.js
+++ src/components/Products/style.js
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const Cards = styled.section`
-    display: grid;
+  display: grid;
+  grid-template-columns: 1fr;
+
+  @media all and (min-width: 992px) {
     grid-template-columns: 1fr 1fr 1fr 1fr;
     gap: 4rem;
     padding: 3rem 0;
-
-    @media all and (min-width: 992px){
-
-    }
+  }
 `;
 
 export const StyledTitle = styled.h2`
