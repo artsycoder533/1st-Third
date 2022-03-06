a87633e5fe31896a9a9fa094096b441e43062cbd
Natasha <njohnson533@gmail.com>
2022-03-06 12:34:39 -0500

    removed typo from href

--- src/components/PriceFilter/PriceFilter.js
+++ src/components/PriceFilter/PriceFilter.js
@@ -2,27 +2,49 @@ import React, { useState, useContext } from "react";
 import { FilterContext } from "../../Contexts/FilterContext";
 
 function PriceFilter({ prices, title }) {
-  const { products, filteredProducts, filterResult, setSelectedPrices, selectedPrices } = useContext(FilterContext);
+  const { products, filteredProducts, filterResult, setSelectedPrices, selectedPrices, setFilteredProducts } = useContext(FilterContext);
   const [isChecked, setIsChecked] = useState(new Array(prices.length).fill(false));
 
   const handleInput = (e, index) => {
     const copyOfIsChecked = [...isChecked];
     copyOfIsChecked[index] = !isChecked[index];
     setIsChecked(copyOfIsChecked);
+
+    
+
+    //if checked
     if (copyOfIsChecked[index]) {
-      console.log(copyOfIsChecked[index]);
-     setSelectedPrices([...selectedPrices, e.currentTarget.name]);
+    //add to list of selected prices
+      setSelectedPrices([...selectedPrices, e.currentTarget.name]);
     }
+
+    
+    //if its been unchecked
     if (copyOfIsChecked[index] === false) {
-      console.log(copyOfIsChecked[index]);
       const copyOfSelectedPrices = [...selectedPrices];
+      //remove from list of selected prices
       if (copyOfSelectedPrices.includes(e.currentTarget.name)) {
-        const newArray = copyOfSelectedPrices.filter(price => {
-          console.log(price, e.currentTarget.name);
+        setSelectedPrices(copyOfSelectedPrices.filter(price => {
           return price !== e.currentTarget.name;
-        });
-         setSelectedPrices(newArray);
+        }));
+         
       }
+    }
+    setProductsToDisplay();
+  }
+
+  const setProductsToDisplay = () => {
+    //filter filteredPRoducts to update whats rendered
+    const copyOfFilteredProducts = [...filteredProducts];
+    //get filter values from selectedPrices array
+    if (selectedPrices.length) {
+      const productsToDisplay = copyOfFilteredProducts.filter((product) => {
+        return (
+          product.price >= selectedPrices[0] - 49.99 &&
+          product.price <= selectedPrices[0]
+        );
+      });
+      setFilteredProducts(productsToDisplay);
     }
   }
   
@@ -36,9 +58,9 @@ function PriceFilter({ prices, title }) {
             <input
               type="checkbox"
               id={lowPrice}
-              value={`${lowPrice}-${highPrice}`}
+              value={highPrice}
               checked={isChecked[index]}
-              name={`${lowPrice}-${highPrice}`}
+              name={highPrice}
               onChange={(e)=>handleInput(e, index)}></input>
             <label htmlFor={lowPrice}>
               ${lowPrice} - ${highPrice}
