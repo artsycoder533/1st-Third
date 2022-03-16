665ae07dae0500a874236fd69c56a839e14e06b4
Natasha <njohnson533@gmail.com>
2022-03-16 01:37:02 -0400

    replaced BrowserRouter with HashRouter

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { GlobalStyle } from "./style";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "../../pages/Home/Home";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -11,7 +11,7 @@ import SingleProductPage from "../../pages/SingleProductPage/SingleProductPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <GlobalStyle />
       <Header />
       <Routes>
@@ -22,7 +22,7 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
