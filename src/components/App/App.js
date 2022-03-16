a1d99fe0acb8e1e0c0b691a5bcee2cf1711566de
Natasha <njohnson533@gmail.com>
2022-03-16 01:46:48 -0400

    removed / from route paths

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,10 +16,10 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/shop/:id" element={<SingleProductPage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<CartPage />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="shop/:id" element={<SingleProductPage />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="cart" element={<CartPage />} />
       </Routes>
       <Footer />
     </HashRouter>
