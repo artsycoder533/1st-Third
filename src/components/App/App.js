f2647ca558852ea7c7536800d955f63582112e4e
Natasha <njohnson533@gmail.com>
2022-03-17 01:09:48 -0400

    added browserrouter back

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ import SingleProductPage from "../../pages/SingleProductPage/SingleProductPage";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <GlobalStyle />
       <Header />
       <Routes>
@@ -22,7 +22,7 @@ function App() {
         <Route path="cart" element={<CartPage />} />
       </Routes>
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
