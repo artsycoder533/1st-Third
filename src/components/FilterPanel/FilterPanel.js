46989e3ae29bb30d7b7afd94ca4118dc222c9e3f
Natasha <njohnson533@gmail.com>
2022-03-18 10:29:18 -0400

    added styling for filter panel

--- src/components/FilterPanel/FilterPanel.js
+++ src/components/FilterPanel/FilterPanel.js
@@ -4,17 +4,19 @@ import PriceFilter from "../PriceFilter/PriceFilter";
 import StarFilter from "../StarFilter/StarFilter";
 import { categories, prices, stars } from "../Filter/filterData";
 import { FilterContext } from "../../Contexts/FilterContext";
+import { Container } from "./style";
+import { PrimaryButton } from "../Button/style";
 
 const FilterPanel = () => {
   const { resetFilters } = useContext(FilterContext);
 
   return (
-    <div>
+    <Container>
       <Filter categories={categories} title={"Category"} />
       <PriceFilter prices={prices} title={"Price"} />
       <StarFilter stars={stars} title={"Customer Ratings"} />
-      <button onClick={resetFilters}>Reset Filters</button>
-    </div>
+      <PrimaryButton onClick={resetFilters}>Reset Filters</PrimaryButton>
+    </Container>
   );
 };
 
--- src/components/FilterPanel/style.js
+++ src/components/FilterPanel/style.js
@@ -1,2 +1,10 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    width: 250px;
+    border: 1px solid lightgrey;
+`;
