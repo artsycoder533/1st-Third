c9161f0f04bd5d4f2600ca27f08344b182f6869e
Natasha <njohnson533@gmail.com>
2022-03-18 10:20:42 -0400

    styled sort select

--- src/components/Sort/style.js
+++ src/components/Sort/style.js
@@ -4,18 +4,23 @@ export const Container = styled.section`
   display: flex;
   width: 100%;
   justify-content: flex-end;
+  padding: 0.5rem 0;
 `;
 
 export const SortForm = styled.form`
-   display: flex;
-   gap: 1rem;
-   align-items: center;
+  display: flex;
+  gap: 1rem;
+  align-items: center;
 
-   select {
-       padding: 0.5rem;
+  select {
+    padding: 0.5rem;
+    cursor: pointer;
+    background: #f5f5f5;
+    font-family: 'Roboto'
+  }
 
-       option {
-           padding: 0.5rem;
-       }
-   }
+  select:focus,
+  select:hover {
+    border: 1px solid #800020;;
+  }
 `;
