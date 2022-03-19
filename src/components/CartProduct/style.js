a405f515b9d7691d696e2200d9536043bd67641a
Natasha <njohnson533@gmail.com>
2022-03-18 22:14:44 -0400

    added media queries to cart page

--- src/components/CartProduct/style.js
+++ src/components/CartProduct/style.js
@@ -2,13 +2,28 @@ import styled from "styled-components";
 import { FiTrash2 } from "react-icons/fi";
 
 export const StyledArticle = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  grid-template-rows: 200px;
+  border-bottom: 1px solid lightgrey;
+  padding: 1rem;
+  gap: 1rem;
+
+  @media all and (min-width: 600px) {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+  }
+
+  @media all and (min-width: 992px) {
     display: grid;
     grid-template-columns: auto 1fr auto;
     grid-template-rows: 200px;
     border-bottom: 1px solid lightgrey;
     padding: 1rem;
-    
     gap: 1rem;
+  }
 `;
 
 export const ImageWrapper = styled.div`
@@ -26,8 +41,14 @@ export const ImageWrapper = styled.div`
 
 export const StyledDetails = styled.div`
   display: grid;
-  grid-template-columns: 2fr 1fr 0.5fr;
+  grid-template-columns: 1fr;
   align-items: center;
+  justify-content: center;
+
+  @media all and (min-width: 1000px) {
+    grid-template-columns: 2fr 1fr 0.5fr;
+    align-items: center;
+  }
 
   div {
     display: flex;
@@ -36,15 +57,21 @@ export const StyledDetails = styled.div`
   }
 
   span {
-    display: block;
+    text-align: center;
+    font-weight: bold;
   }
 `;
 
 export const RemoveButton = styled(FiTrash2)`
-  align-self: center;
+  align-self: flex-end;
   font-size: 1.5rem;
   color: red;
   cursor: pointer;
+  text-align: center;
+
+  @media all and (min-width: 1000px) {
+    align-self: center;
+  }
 `;
 
 export const CountButton = styled.button`
