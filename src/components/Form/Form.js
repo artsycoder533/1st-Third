cc8f332143ef3f202b6a61716da1ab87e45ea2a3
Natasha <njohnson533@gmail.com>
2022-03-27 01:21:50 -0400

    added form validation to contat page

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,93 @@
-import React from 'react'
-import { StyledForm, InputContainer } from './style';
+import React, {useState} from 'react'
+import { StyledForm, InputContainer, FormButton } from './style';
 import { PrimaryButton } from '../Button/style';
+import FormInput from '../FormInput/FormInput';
 
 function Form() {
+  const [formFields, setFormFields] = useState({
+    nameInput: "",
+    emailInput: "",
+    messageInput: ""
+  });
+  const [errors, setErrors] = useState({
+    nameError: "",
+    emailError: "",
+    messageError: ""
+  });
+
+  const handleInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormFields({
+      [name]: value
+    });
+  }
+
+  const validate = () => {
+    let nameErr, emailErr, messageErr;
+    let status;
+
+    if (nameInput.trim() === "" || nameInput.match(/\d/)) {
+      nameErr = "Enter a valid name";
+      status = false;
+    }
+    if (
+      emailInput.trim() === "" ||
+      !emailInput.match(/^([a-z\d\.-_]+)@([a-z\d\.-_]+)\.([a-z]{2,})$/)
+    ) {
+      emailErr = "Enter a valid email";
+      status = false;
+    }
+    if (messageInput.trim() === "") {
+      messageErr = "Message cannot be blank";
+      status = false;
+    }
+    setErrors({
+      nameError: nameErr,
+      emailError: emailErr,
+      messageError: messageErr
+    });
+
+    if (status) {
+      setFormFields({
+        nameInput: "",
+        emailInput: "",
+        messageInput: ""
+      })
+    }
+  }
+
+  const { nameInput, emailInput, messageInput } = formFields;
+  const { nameError, emailError, messageError } = errors;
+
   return (
-      <StyledForm>
-          <h3>Send Us A Message!</h3>
+    <StyledForm action="https://formspree.io/f/mvolbzdo" method="POST">
+      <h3>Send Us A Message!</h3>
       <InputContainer>
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" required />
+        <FormInput
+          htmlFor="name"
+          type="text"
+          id="name"
+          name="name"
+          label="Name:"
+          value={nameInput}
+          onChange={handleInput}
+          required
+        />
+        <span>{nameError}</span>
       </InputContainer>
       <InputContainer>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" required />
+        <FormInput
+          htmlFor="email"
+          type="email"
+          id="email"
+          name="email"
+          label="Email:"
+          value={emailInput}
+          onChange={handleInput}
+          required
+        />
+        <span>{emailError}</span>
       </InputContainer>
       <InputContainer>
         <label htmlFor="message">Message</label>
@@ -21,9 +96,12 @@ function Form() {
           id="message"
           cols="30"
           rows="10"
-          required></textarea>
-          </InputContainer>
-          <PrimaryButton>Send Message</PrimaryButton>
+          value={messageInput}
+          onChange={handleInput}
+        required></textarea>
+        <span>{messageError}</span>
+      </InputContainer>
+      <FormButton type="submit" onClick={validate}>Send Message</FormButton>
     </StyledForm>
   );
 }
--- src/components/Form/style.js
+++ src/components/Form/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { PrimaryButton } from '../Button/style';
 
 export const StyledForm = styled.form`
   display: flex;
@@ -8,14 +9,27 @@ export const StyledForm = styled.form`
   margin: 0 auto;
   padding: 2rem;
   gap: 1rem;
-  
 `;
 
 export const InputContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    
-    input {
-        padding: 0.75rem;
-    }
+  display: flex;
+  flex-direction: column;
+
+  input {
+    padding: 0.75rem;
+  }
+
+  span {
+    color: red;
+  }
+
+  textarea {
+    padding: 0.75rem;
+    font-family: "Roboto", sans-serif;
+    font-size: 1rem;
+  }
+`;
+
+export const FormButton = styled(PrimaryButton)`
+  align-self: flex-end;
 `;
